e928dd67519842182d3c9a3820605bbcc1f223da
Michael Rozenblat <michael.rozenblat@pendo.io>
2023-05-08 13:56:30 -0400

    forcing it to work

---
+++ Sources/URLUtils/ObjcFile.m
@@ -0,0 +1,12 @@
+//
+//  ObjcFile.m
+//  
+//
+//  Created by Michael Rozenblat on 08/05/2023.
+//
+
+#import "ObjcFile.h"
+
+@implementation ObjcFile
+
+@end
---
+++ Sources/URLUtils/include/ObjcFile.h
@@ -0,0 +1,16 @@
+//
+//  ObjcFile.h
+//  
+//
+//  Created by Michael Rozenblat on 08/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjcFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
